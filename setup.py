422bf987ff99f6a70ccf6911447b5a9484c4b9a7
James Ridgway <myself@james-ridgway.co.uk>
2017-08-12 16:07:42 +0100

    Updated descrip[tion

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
 from setuptools import setup
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
-
 setup(
     name='attachment-downloader',
     version='1.0.1',
     description='Simple tool for downloading email attachments for all emails in a given folder using an IMAP client.',
-    long_description=read_md('README.md'),
+    long_description=open('README.rst').read(),
     author='James Ridgway',
     url='https://github.com/jamesridgway/attachment-downloader',
     license='MIT',
