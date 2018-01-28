c82117a256ca377433597c057b9d83b107c3c0f6
James Ridgway <myself@james-ridgway.co.uk>
2018-01-28 12:20:43 +0000

    1.1.1 Release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='attachment-downloader',
-    version='1.1.0',
+    version='1.1.1',
     description='Simple tool for downloading email attachments for all emails in a given folder using an IMAP client.',
     long_description=open('README.rst').read(),
     author='James Ridgway',
