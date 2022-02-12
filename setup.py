781f583e5298d23a25db9196d32b0a6eacaefd15
James Ridgway <james@jamesridgway.co.uk>
2022-02-12 13:51:12 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='attachment-downloader',
-    version='1.1.6',
+    version='1.1.7',
     description='Simple tool for downloading email attachments for all emails in a given folder using an IMAP client.',
     long_description=open('README.rst').read(),
     author='James Ridgway',
