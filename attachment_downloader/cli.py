4468e427de56f5aa058d43e92109058869b9edeb
James Ridgway <james@jamesridgway.co.uk>
2022-03-05 14:47:44 +0000

    Initial linting and testing improvements

--- attachment_downloader/cli.py
+++ attachment_downloader/cli.py
@@ -11,11 +11,10 @@ def valid_date(option, opt, value):
             parsed_value = parsed_value.replace(tzinfo=datetime.timezone.utc)
         return parsed_value
     except ValueError:
-        raise OptionValueError('option %s: invalid date format: %r' % (opt, value))
+        raise OptionValueError(f'option {opt}: invalid date format: {value}')
 
 
 def get_password():
     if sys.stdin.isatty():
         return getpass('IMAP Password: ')
-    else:
-        return sys.stdin.read().strip()
+    return sys.stdin.read().strip()
---
+++ run-tests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./venv/bin/pytest --cov-report html --cov=attachment_downloader tests
